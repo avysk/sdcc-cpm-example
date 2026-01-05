3269e8c61db1b19fca0b574cc2af735c34f6069c
Alexey Vyskubov <alexey@ocaml.nl>
2026-01-05 13:04:33 +0200

    remove unneeded code

--- src/main.c
+++ src/main.c
@@ -1,14 +1,7 @@
-#include <stdio.h>
-
 /* function defined in another translation unit */
 void greet(void);
 
 int main(void) {
   greet();
-  __asm
-	ld c, #0
-	call 5
-  __endasm;
   return 0;
 }
-
